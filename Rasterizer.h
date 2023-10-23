5c226ebb64b54d259275c54f35cd39407e104ec2
neeewbieee <3043818081@qq.com>
2023-10-23 20:07:16 +0800

    add additional information

--- Rasterizer.h
+++ Rasterizer.h
@@ -1,36 +1,41 @@
 #ifndef RASTERIZER_HEADER
 #define RASTERIZER_HEADER
 
-#include"Triangle.h"
-#include"Camera.h"
+#include "Camera.h"
+#include "Triangle.h"
+#include <cfloat>
 
 namespace rst {
 
-	using namespace Eigen;
+using namespace Eigen;
 
-	class rasterizer {
+class rasterizer {
 
-	public:
-		rst::camera* CAMERA;
-		std::vector<float>depth_buffer;
-		std::vector<Vector3f>frame_buffer;
-		std::vector<light>lights;
-		std::function<Vector3f(rasterizer*, const payload&)> get_texture = &rasterizer::direct_sample;
+public:
+  rst::camera *CAMERA;
+  std::vector<float> depth_buffer;
+  std::vector<Vector3f> frame_buffer;
+  std::vector<light> lights;
+  std::function<Vector3f(rasterizer *, const payload &)> get_texture =
+      &rasterizer::direct_sample;
 
-		rasterizer(rst::camera* _CAMERA);
-		size_t get_index(int x, int y);
-		void set_lights(const std::vector<light>& _lights);
-		void draw(std::vector<triangle>& list);
-		const Vector4f& vertex_shader(const payload& vertex_in_payload);
-		void rasterizer_triangle(const triangle& tri, const std::array<Vector4f, 3>& view_space_vertex,std::vector<light> lights_to_rst);
-		Vector3f fragment_shader(const payload& load, std::vector<light> lights_to_rst);
-		Vector3f direct_sample(const payload& load);
-		Vector3f Bilinear_interpolation(const payload& load);
-		void load_dword(DWORD* frame);
-		void load_SSAA(DWORD* frame);
-		void sky_box();
-		void clear();
-	};
-}
+  rasterizer(rst::camera *_CAMERA);
+  size_t get_index(int x, int y);
+  void set_lights(const std::vector<light> &_lights);
+  void draw(std::vector<triangle> &list);
+  const Vector4f &vertex_shader(const payload &vertex_in_payload);
+  void rasterizer_triangle(const triangle &tri,
+                           const std::array<Vector4f, 3> &view_space_vertex,
+                           std::vector<light> lights_to_rst);
+  Vector3f fragment_shader(const payload &load,
+                           std::vector<light> lights_to_rst);
+  Vector3f direct_sample(const payload &load);
+  Vector3f Bilinear_interpolation(const payload &load);
+  void load_dword(DWORD *frame);
+  void load_SSAA(DWORD *frame);
+  void sky_box();
+  void clear();
+};
+} // namespace rst
 
-#endif //RASTERIZER_HEADER
+#endif // RASTERIZER_HEADER
--- readme.md
+++ readme.md
@@ -1,5 +1,17 @@
+A modern cpp simple software rasterizer based on `easyx`(for reading and presenting img)
+and `eigen3`(for matrix calculation).
+
 ## Prerequisite:
 > visual studio 2022<br>
 > eigen3<br>
-> easyx
+> easyx<br>
+> openmp
 
+## Features:
+ - [x] triangle
+ - [x] skybox
+ - [x] SSAA
+ - [x] texture
+
+## Result:
+![image](cube.jpg)
